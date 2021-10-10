58af46b9c8e68f6ce667d5cb62904cfc94eeb8a4
ShiftTGC <ShiftTGC@gmail.com>
2021-10-11 00:00:07 +0200

    Add project files.

---
+++ FizzBuzz/Marathon.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FizzBuzz.Program;
+
+namespace FizzBuzz
+{
+    class Marathon
+    {
+        /// <summary>
+        /// Currently there is only one player, but if there will be more, maybe something to figure out later.
+        /// </summary>
+        public static void OnePlayer()
+        {
+            bool loop2;
+            loop2 = true;
+            string input = null;
+            while (loop2 == true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("" +
+                        $"Hello, {Environment.UserName}!\n" +
+                        "Welcome to FizzBuzz Marathon mode!\n" +
+                        "\n" +
+                        "Please enter from which number you want to start from:");
+                    int number = Convert.ToInt32(input = Console.ReadLine());
+                    Console.WriteLine("Have fun!");
+                    loop2 = false;
+                    System.Threading.Thread.Sleep(1000);
+                    PlayGame(number);
+                }
+                catch
+                {
+                    IntegerError(input, 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initiates the game (for OnePlayer). Requires a integer number which will be the input
+        /// </summary>
+        /// <param name="number"></param>
+        static void PlayGame(int number)
+        {
+            string checkMe = null;
+            int checkMeNumber;
+            int currentNumber = number;
+            int startingNumber = number;
+            int score;
+            string unecesaryDetails = "";
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.Write($"" +
+                $"--------------------------------------------------\n" +
+                $"|FizzBuzz Marathon!                              |\n" +
+                $"|Current number is: {currentNumber}              |\n" +
+                $"--------------------------------------------------\n" +
+                $"Fizz, Buzz, FizzBuzz or <number>?\n" +
+                $"Answer: ");
+            ResetHUD(currentNumber);
+
+            for (bool mistakeMade = false; mistakeMade == false;)
+            {
+                checkMe = Console.ReadLine();
+
+                if (checkMe == Convert.ToString(currentNumber))
+                {
+                    checkMeNumber = currentNumber;
+                    if (CheckCorrectness(checkMeNumber) == false) { mistakeMade = true; break; }
+                }
+                else if (checkMe.ToLower() != CheckFizzBuzz(currentNumber).ToLower()) { mistakeMade = true; break; }
+
+                currentNumber++;
+                ResetHUD(currentNumber);
+            }
+            string failReason = CheckFizzBuzz(currentNumber);
+            if (failReason == "useNumber") failReason = Convert.ToString(currentNumber);
+            Console.SetCursorPosition(0, 4);
+            Console.WriteLine("Aaaaah, sorry about that, but that was wrong!");
+            Console.WriteLine($"The Correct answer was: \"{failReason}\"");
+            score = currentNumber - startingNumber;
+
+            if (score == 0)
+            {
+                Console.WriteLine("\n" +
+                    "Miss-types happens. And it sucks when it happens.\n" +
+                    "At least it didn't happen while you were on a roll.\n" +
+                    "\n" +
+                    "In case you are unsure what the rules are, in the main menu, please type \"3\" or \"rules\" to view the rules.\n" +
+                    "Additionally, right under the box that say you are playing Marathon mode, you get the \"permitted alternatives\".\n" +
+                    "\n" +
+                    "\"Fizz, Buzz, FizzBuzz or <number>?\" - These are the options you have to type in.\n" +
+                    "(And no, you can type in lowercase, uppercase or a combination, exception being the number, which has to be an integer.)\n" +
+                    "\n" +
+                    "You may think \"why not just give me options like in the main menu?\"\n" +
+                    "Well... I was considering it, but then I realized: You don't have a timer.\n" +
+                    "So, in the end, I decided against it, for the sake of the challenge.");
+                Console.WriteLine("\n" +
+                    "Click any button on your keyboard to return to menu...");
+                Console.ReadKey();
+                Console.Clear();
+                Title();
+            }
+            else
+            {
+                if (score != 1) unecesaryDetails = "s";
+                Console.WriteLine("\n" +
+                    $"You started from: {startingNumber}\n" +
+                    $"And successfully knew/guessed {score} number{unecesaryDetails} from thereon!\n" +
+                    $"Your score is therefore: {score}");
+                Console.WriteLine($"\nThanks for playing, {Environment.UserName}!");
+                Console.WriteLine("\n" +
+                    "Click any button on your keyboard to return to menu...");
+                Console.ReadKey();
+                Console.Clear();
+                Title();
+            }
+        }
+
+        static bool CheckCorrectness(int number)
+        {
+            if ("useNumber" == CheckFizzBuzz(number)) return true;
+            return false;
+        }
+
+
+        static void ResetHUD(int currentNumber)
+        {
+            {
+                Console.SetCursorPosition(8, 5);
+                Console.Write("                                                    ");
+                Console.SetCursorPosition(20, 2);
+                Console.Write("                                                    ");
+                Console.SetCursorPosition(49, 2);
+                Console.Write("|");
+                Console.SetCursorPosition(20, 2);
+                Console.Write(currentNumber);
+                Console.SetCursorPosition(24, 4);
+                Console.Write("                                                    ");
+                Console.SetCursorPosition(24, 4);
+                Console.Write($"{currentNumber}?");
+                Console.SetCursorPosition(8, 5);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using static FizzBuzz.Marathon;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rules of FizzBuzz: (WikiPedia)
+            //Players generally sit in a circle.
+            //The player designated to go first says the number "1", and the players then count upwards in turn.
+            //However, any number divisible by three is replaced by the word fizz
+            //Any number divisible by five by the word buzz.
+            //Numbers divisible by 15 become fizz buzz.
+            //A player who hesitates or makes a mistake is eliminated from the game.
+            Title();
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input.ToLower() == "2" || input.ToLower() == "play")  OnePlayer();
+                else if (input.ToLower() == "1" || input.ToLower() == "generate")
+                {
+                    bool loop = true;
+                    while (loop == true)
+                    {
+                        try
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Let us generate a number!");
+                            Console.WriteLine("Please input the smallest/start number.");
+                            Console.Write("Smallest Number: ");
+                            input = Console.ReadLine();
+                            int start = Int32.Parse(input);
+                            Console.SetCursorPosition(0, 1);
+                            Console.WriteLine("Please input the largest/end number.          ");
+                            Console.WriteLine("                                         ");
+                            Console.SetCursorPosition(0, 2);
+                            Console.Write("Biggest Number: ");
+                            input = Console.ReadLine();
+                            int end = Int32.Parse(input);
+                            Console.Clear();
+
+                            SequenticFizzBuzzGenerator(start, end);
+                            loop = false;
+
+                            Console.WriteLine("\n" +
+                                "Click any button on your keyboard to return to menu...");
+                            Console.ReadKey();
+                            Title();
+                        }
+                        catch
+                        {
+                            IntegerError(input, 1);
+                            Title();
+                        }
+                    }
+                }
+
+                else if (input.ToLower() == "3" || input.ToLower() == "rules")
+                {
+                    Console.Clear();
+                    Console.WriteLine("" +
+                    "Rules of FizzBuzz: (WikiPedia)\n" +
+                    "Players generally sit in a circle.\n" +
+                    "The player designated to go first says the number \"1\", and the players then count upwards in turn.\n" +
+                    "However, any number divisible by three (3) is replaced by the word Fizz\n" +
+                    "Any number divisible by five (5) by the word Buzz.\n" +
+                    "Numbers divisible by fifteen (15) become FizzBuzz.\n" +
+                    "A player who hesitates or makes a mistake is eliminated from the game.\n" +
+                    "(Although the playable version in this exe does not have a timer and technically no multiplayer.)");
+                    Console.WriteLine("\n" +
+                        "Click any button on your keyboard to return...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Title();
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, 3);
+                    Console.WriteLine("                                    ");
+                    Console.SetCursorPosition(0, 2);
+                    Console.WriteLine("Please choose between 1, 2 or 3.");
+                }
+            }
+        }
+
+        /// <summary>
+        ///Generates a sequence of numbers, Fizz, Buzz and FizzBuzz from 0 to a number of your choice.
+        /// </summary>
+        /// <param name="end"></param>
+        static void SequenticFizzBuzzGenerator(int end)
+        {
+            SequenticFizzBuzzGenerator(0, end);
+        }
+
+        /// <summary>
+        /// Generates a sequence of numbers, Fizz, Buzz and FizzBuzz between the numbers you have given it.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        static void SequenticFizzBuzzGenerator(int start, int end)
+        {
+            if (start > end)
+            {
+                Console.WriteLine("You did the reverse of what you were supposed to.\n" +
+                    $"You made the \"Biggest Number\" lower than the \"Lowest Number\" (LN: {start}, BN: {end})\n" +
+                    "Just imagine what would have happened to the program if I hadn't caught this!\n" +
+                    "You could have KILLED my program!... GLaDOS were right... You are monsters...");
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            {
+                for (int i = start; i < end + 1; i++)
+                {
+                    string result = CheckFizzBuzz(i);
+                    if (result == "useNumber")
+                        Console.WriteLine(i);
+                    else Console.WriteLine(result);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks for Fizz, Buzz, FizzBuzz or if normal number shall be used or not.
+        /// Seperated for potential player-engagment game-mode in the future.
+        /// </summary>
+        /// <param name="finput"></param>
+        public static string CheckFizzBuzz(int finput)
+        {
+            if (finput == 0) return "useNumber";
+            else if (finput % 15 == 0) return "FizzBuzz";
+            else if (finput % 3 == 0) return "Fizz";
+            else if (finput % 5 == 0) return "Buzz";
+            else return "useNumber";
+        }
+
+        public static void Title()
+        {
+            Console.Clear();
+            Console.WriteLine("Want to generate, play or view the rules of the game?");
+            Console.WriteLine("(1=generate, 2=play, 3=rules)");
+            Console.SetCursorPosition(0, 5);
+            Console.WriteLine("Code put together by ShiftTGC\n" +
+                "\"Yes, I made this because of Tom Scott's FizzBuzz video.\"");
+            Console.SetCursorPosition(0, 2);
+        }
+        public static void IntegerError(string input, int errorCode)
+        {
+            Console.Clear();
+            Console.WriteLine("ERROR: Oopsie woopsie, someone did a fucky wucky~ UwU\n");
+            if (errorCode == 1) { Console.WriteLine("Unlike the Main Menu, the \"Lowest Number\" & \"Biggest Number\" has to be integers.\n"); }
+            if (errorCode == 2) { Console.WriteLine("Unlike the Main Menu, your starting number has to be an integer.\n"); }
+            Console.WriteLine("" +
+                "If you don't know, an integer is a whole number, like \"1\", \"2\", \"3\",\"10\", \"20\", \"30\", \"123\" and so on.\n" +
+                $"\"1.01\",\"0.99\", letters, words, commas, symbols and what you typed: \"{input}\"\n" +
+                "These are NOT integer(s).\n" +
+                "\n" +
+                "Press a key to try again, but please use integers this time...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
